a7985e47cd4566b82563db550ca002fce12f83b8
Matthew <evilnops@gmail.com>
2017-06-25 19:03:43 +0800

    Extend `RxPaginationProvider` to conform protocol `PageableType`

--- OctoKit/Client/Provider/RxPaginationProvider.swift
+++ OctoKit/Client/Provider/RxPaginationProvider.swift
@@ -16,3 +16,72 @@ public class RxPaginationProvider<Target>: RxMoyaProvider<Target> where Target: 
     
 }
 
+// MARK: - Pagination
+
+extension RxPaginationProvider: PageableType {
+    
+    public func perPage(with perPage: Int) -> Int {
+        return 1...100 ~= perPage ? perPage : 30
+    }
+    
+    public func page(with offset: Int, perPage: Int) -> Int {
+        return offset / perPage + 1
+    }
+    
+    public func pageOffset(_ offset: Int, perPage: Int) -> Int {
+        return offset % perPage
+    }
+    
+    public func nextPageURL(from response: HTTPURLResponse) -> URL? {
+        if let linkHeader = response.allHeaderFields["Link"] as? String {
+            /* looks like:
+             <https://api.github.com/user/20267/gists?page=2>; rel="next",
+             <https://api.github.com/user/20267/gists?page=6>; rel="last"
+             */
+            // so split on "," then on ";"
+            let componets = linkHeader.characters.split { $0 == "," }.map { String($0) }
+            
+            // now we have 2 lines like
+            // '<https://api.github.com/user/20267/gists?page=2>; rel="next"'
+            // So let's get the URL out of there:
+            for item in componets {
+                // see if it's "next"
+                let rangeOfNext = item.range(of: "rel=\"next\"")
+                
+                if rangeOfNext != nil {
+                    let rangeOfPaddedURL = item.range(
+                        of: "<(.*)>;", options: .regularExpression, range: nil, locale: nil
+                    )
+                    
+                    if let range = rangeOfPaddedURL {
+                        let nextURL = item.substring(with: range)
+                        
+                        // strip off the < and >;
+                        let startIndex = nextURL.index(nextURL.startIndex, offsetBy: 1)
+                        let endIndex = nextURL.index(nextURL.endIndex, offsetBy: -2)
+                        let URLRange = startIndex..<endIndex
+                        
+                        return URL(string: nextURL.substring(with: URLRange))
+                    }
+                }
+            }
+        }
+        
+        return nil
+    }
+    
+    public func nextPageNumber(from response: HTTPURLResponse) -> Int? {
+        guard let nextPageURL = nextPageURL(from: response) else {
+            return nil
+        }
+        
+        let components = URLComponents(url: nextPageURL, resolvingAgainstBaseURL: false)
+        
+        if let pageNumberString = components?.queryValue(for: "page") {
+            print("Page number: \(pageNumberString)")
+            return Int(pageNumberString)
+        }
+        
+        return nil
+    }
+}
