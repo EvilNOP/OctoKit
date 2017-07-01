8135027ce57d10bf17a644a3e1adf3f3fa4d2399
Matthew <evilnops@gmail.com>
2017-07-01 10:03:01 +0800

    Resolved ambiguity on calling the method `fetchUserReceivedEvents`

--- OctoKit/Events/Client+Events.swift
+++ OctoKit/Events/Client+Events.swift
@@ -18,35 +18,18 @@ public extension Client {
     /// the latest data matches `etag`, the call does not count toward the API rate
     /// limit.
     ///
-    /// - parameter etag: An Etag from a previous request, used to avoid downloading
-    ///                   unnecessary data.
-    ///
-    /// - returns: A signal which will send zero or more Responses (of Events) if
-    ///            new data was downloaded. Unrecognized events will be omitted from the result.
-    ///            On success, the signal will send completed regardless of whether there was
-    ///            new data. If no `user` is set, the signal will error immediately.
-    public func fetchUserReceivedEvents(_ notMatchingEtag: String? = nil,
-                                        fetchAllPages: Bool = false) -> Observable<Event> {
-        guard let user = self.user, let login = user.login else {
-            return Observable.error(ClientError.userRequired(ClientError.userNameRequiredError))
-        }
-        
-        return provider.request(
-            MultiTarget(EventsRouter.userReceivedEvents(EventsParameter(login: login))),
-            fetchAllPages: fetchAllPages
-        )
-    }
-    
-    /// Fetches the received events of the current user.
-    ///
     /// - parameter offset:  Allows you to specify an offset at which items will begin being
     ///                      returned.
     ///
     /// - parameter perPage: The perPage parameter. You can set a custom page size up to 100 and
     ///                      the default value 30 will be used if you pass 0 or greater than 100.
     ///
+    /// - parameter etag:    An Etag from a previous request, used to avoid downloading
+    ///                      unnecessary data.
+    ///
     /// - returns: A signal which sends zero or more Event objects.
     public func fetchUserReceivedEvents(with offset: Int = 0, perPage: Int = 30,
+                                        notMatchingEtag etag: String? = nil,
                                         fetchAllPages: Bool = false) -> Observable<Event> {
         guard let user = self.user, let login = user.login else {
             return Observable.error(ClientError.userRequired(ClientError.userNameRequiredError))
